93616b59105f585e4e3ed05d16310fc988b0d949
Xiuhong <miaozi1123@126.com>
2019-06-13 14:40:43 +0800

    Update MsgBox.java
    update style

--- src/android/MsgBox.java
+++ src/android/MsgBox.java
@@ -4,11 +4,18 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import com.net263.videoconference.R;
 import org.apache.cordova.LOG;
 
 import java.util.HashMap;
@@ -41,17 +48,24 @@ public class MsgBox {
         if (noticeDialog == null) {
             LOG.d(TAG, "showNoticeDialog");
             // 构造对话框
-            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            Builder builder = new Builder(mContext,R.style.UpdateDialogStyle);
+
             builder.setTitle(msgHelper.getString(MsgHelper.UPDATE_TITLE));
+
             builder.setMessage(msgHelper.getString(MsgHelper.UPDATE_MESSAGE));
             // 更新
             builder.setPositiveButton(msgHelper.getString(MsgHelper.UPDATE_UPDATE_BTN), onClickListener);
+
             noticeDialog = builder.create();
+
         }
 
         if (!noticeDialog.isShowing()) noticeDialog.show();
+        TextView textView = (TextView) noticeDialog.findViewById(android.R.id.message);
+        textView.setGravity(Gravity.CENTER);
+        textView.setPadding(0,20,0,0);
+        noticeDialog.setCancelable(false);// 设置点击屏幕Dialog不消失
 
-        noticeDialog.setCanceledOnTouchOutside(false);// 设置点击屏幕Dialog不消失
         return noticeDialog;
     }
 
@@ -67,7 +81,7 @@ public class MsgBox {
             LOG.d(TAG, "showDownloadDialog");
 
             // 构造软件下载对话框
-            AlertDialog.Builder builder = new Builder(mContext);
+            AlertDialog.Builder builder = new Builder(mContext,AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
             builder.setTitle(msgHelper.getString(MsgHelper.UPDATING));
             // 给下载对话框增加进度条
             final LayoutInflater inflater = LayoutInflater.from(mContext);
@@ -76,23 +90,22 @@ public class MsgBox {
             /* 更新进度条 */
             downloadDialogProgress = (ProgressBar) v.findViewById(msgHelper.getId(MsgHelper.UPDATE_PROGRESS));
             builder.setView(v);
-            // 取消更新
-            //builder.setNegativeButton(msgHelper.getString("update_cancel"), onClickListener);
-            //转到后台更新
-            builder.setNegativeButton(msgHelper.getString(MsgHelper.UPDATE_BG), onClickListenerNeg);
             builder.setNeutralButton(msgHelper.getString(MsgHelper.DOWNLOAD_COMPLETE_NEU_BTN), onClickListenerNeu);
             builder.setPositiveButton(msgHelper.getString(MsgHelper.DOWNLOAD_COMPLETE_POS_BTN), onClickListenerPos);
+
             downloadDialog = builder.create();
         }
 
         if (showDialog && !downloadDialog.isShowing()) downloadDialog.show();
 
         downloadDialog.setTitle(msgHelper.getString(MsgHelper.UPDATING));
-        downloadDialog.setCanceledOnTouchOutside(false);// 设置点击屏幕Dialog不消失
+        downloadDialog.setCancelable(false);// 设置点击屏幕Dialog不消失
         if (downloadDialog.isShowing()) {
             downloadDialog.getButton(DialogInterface.BUTTON_NEGATIVE).setVisibility(View.VISIBLE); //Update in background
             downloadDialog.getButton(DialogInterface.BUTTON_NEUTRAL).setVisibility(View.GONE); //Install Manually
+            downloadDialog.getButton(DialogInterface.BUTTON_NEUTRAL).setTextColor(Color.parseColor("#0078d7"));
             downloadDialog.getButton(DialogInterface.BUTTON_POSITIVE).setVisibility(View.GONE); //Download Again
+            downloadDialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.parseColor("#0078d7"));
         }
 
         Map<String, Object> ret = new HashMap<String, Object>();
@@ -110,7 +123,7 @@ public class MsgBox {
         if (this.errorDialog == null) {
             LOG.d(TAG, "initErrorDialog");
             // 构造对话框
-            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext,AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
             builder.setTitle(msgHelper.getString(MsgHelper.UPDATE_ERROR_TITLE));
             builder.setMessage(msgHelper.getString(MsgHelper.UPDATE_ERROR_MESSAGE));
             // 更新
@@ -119,7 +132,6 @@ public class MsgBox {
         }
 
         if (!errorDialog.isShowing()) errorDialog.show();
-
         return errorDialog;
     }
 
